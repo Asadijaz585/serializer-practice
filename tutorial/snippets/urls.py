9999181e80c259b7ebfaf7d32d3a952da846ccd7
Asad Ijaz <asad.ijaz585@gmail.com>
2023-02-08 20:06:57 +0500

    Error Handling

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from snippets import views
+from snippets.views import filter_list, search
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
+    path('snippets/filter/<str:pk>/', filter_list.as_view()),
+    path('snippets/search/<str:pk>/', search.as_view())
 ]
