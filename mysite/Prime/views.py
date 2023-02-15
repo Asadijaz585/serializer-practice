e8677bdc1fc627bc86bdc803871b8c77db1f97a8
Asad Ijaz <asad.ijaz585@gmail.com>
2023-02-15 20:15:31 +0500

    Tutorial 4

--- mysite/Prime/views.py
+++ mysite/Prime/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from .models import Question
+from django.http import Http404
 
-# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    
+    # template = loader.get_template('D:/Projects/Scrap-Data/Current Work Space/Django/mysite/Prime/templates/prime/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+
+    context = {'latest_question_list': latest_question_list} # render, instead of loader
+    return render(request, 'D:/Projects/Scrap-Data/Current Work Space/Django/mysite/Prime/templates/prime/index.html', context)
+
+def detail(request, question_id):
+    # return HttpResponse("You're looking at question %s." % question_id)
+
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'D:/Projects/Scrap-Data/Current Work Space/Django/mysite/Prime/templates/prime/detail.html', {'question': question})
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'D:/Projects/Scrap-Data/Current Work Space/Django/mysite/Prime/templates/prime/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
